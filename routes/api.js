3bbe67f7e2a0e460a3d94382a0de5b6c4088c125
amilosevic129 <a.rmilosevic129@gmail.com>
2019-05-03 20:16:41 +0800

    route and contrl commit

--- routes/api.js
+++ routes/api.js
@@ -2,15 +2,15 @@
 'use strict';
  
 var express = require('express');
-var ApiController  = require('../controllers/apiContrl');
+// var ApiController  = require('../controllers/apiContrl');
 // Sett Routing 
 var router = express.Router();
 
 // connect page => to instagram by ajax.
-router.post('/connectinstagram', ApiController.connectToinstagram);
-router.post('/savetags', ApiController.saveHastags)
-router.get('/savecomment',ApiController.saveComment)
-router.get('/savereply',ApiController.saveReply)
-router.get('/savemaxcmmdaily', ApiController.saveMaxCmmDaily)
-router.get('/postcomment', ApiController.postComment)
+// router.post('/connectinstagram', ApiController.connectToinstagram);
+// router.post('/savetags', ApiController.saveHastags)
+// router.get('/savecomment',ApiController.saveComment)
+// router.get('/savereply',ApiController.saveReply)
+// router.get('/savemaxcmmdaily', ApiController.saveMaxCmmDaily)
+// router.get('/postcomment', ApiController.postComment)
 module.exports = router;
